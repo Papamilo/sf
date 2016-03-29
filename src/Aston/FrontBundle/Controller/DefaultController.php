c7d1149c13fd53f63df99122cd4f53569ec18b46
Vincent Letourneau <vincent@nanoninja.com>
2016-03-29 12:52:03 +0200

    Ajout AstonFrontBunlde:Default:contact

--- src/Aston/FrontBundle/Controller/DefaultController.php
+++ src/Aston/FrontBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace Aston\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AstonFrontBundle:Default:index.html.twig');
+    }
+
+    public function contactAction(Request $req)
+    {
+        $fb = $this->createFormBuilder();
+        return $this->render('AstonFrontBundle:Default:contact.html.twig');
     }
 }
